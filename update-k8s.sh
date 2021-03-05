1fdbf954c786cb18ea7188c088e70e2272b3f87e
kingdonb <kingdonb@users.noreply.github.com>
2021-03-05 15:59:08 +0000

    pretty nice build example
    any-old-app-version
    
    "app-version" is not descriptive enough if this config must live in the
    flux-system namespace
    
    We'll have to move the any-old-app Kustomization into the devl namespace
    take this responsibility away from flux-system
    we will provide this in the app repo
    
    kustomization is part of config

--- update-k8s.sh
+++ update-k8s.sh
@@ -4,4 +4,4 @@ set -feuo pipefail
 
 GIT_SHA=${1:0:8}
 sed -i "s|image: kingdonb/any-old-app:.*|image: kingdonb/any-old-app:$GIT_SHA|" k8s.yml
-sed -i "s|git-sha: .*|git-sha: $GIT_SHA|" flux/app-version-configmap.yaml
+sed -i "s|GIT_SHA: .*|GIT_SHA: $GIT_SHA|" flux/app-version-configmap.yaml
