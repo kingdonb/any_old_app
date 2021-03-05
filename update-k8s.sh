a3ea0b5140b84f6d7c3765db3f300d23ed44ec2b
Kingdon Barrett <kingdon@weave.works>
2021-03-05 09:22:28 -0500

    (gnu) sed -i does not take an argument

--- update-k8s.sh
+++ update-k8s.sh
@@ -3,5 +3,5 @@
 set -feuo pipefail
 
 GIT_SHA=${1:0:8}
-sed -i '.backup' "s|image: kingdonb/any-old-app:.*|image: kingdonb/any-old-app:$GIT_SHA|" k8s.yml
-sed -i '.backup' "s|git-sha: .*|git-sha: $GIT_SHA|" flux/app-version-configmap.yaml
+sed -i "s|image: kingdonb/any-old-app:.*|image: kingdonb/any-old-app:$GIT_SHA|" k8s.yml
+sed -i "s|git-sha: .*|git-sha: $GIT_SHA|" flux/app-version-configmap.yaml
